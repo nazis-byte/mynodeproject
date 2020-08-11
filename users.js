829e08294e70e16c907d8d815d4c63e8fe04e433
nazis-byte <57666642+nazis-byte@users.noreply.github.com>
2020-08-11 13:11:17 +0530

    Add files via upload

--- users.js
+++ users.js
@@ -1,31 +1,8 @@
-const Users = require("../schemas/users");
-let loginUser = async (request) => {
-  console.log(request, "req")
-  const userinfo = await Users.find({ email: request.email, password: request.password })
-  console.log(userinfo,)
-  return userinfo
-}
-
-
-
-let registerUser = async (params) => {
-  let query = {
-    name: params.name,
-    email: params.email,
-    password: params.password,
-  }
-  var addusers = new Users(query);
-  var result = await addusers.save();
-  return result;
-}
-
-
-let userList = async () => {
-  const userdata = await Users.find()
-  return userdata
-}
-module.exports = {
-  loginUser,
-  registerUser,
-  userList
-};
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const UserSchema = new Schema({
+    name: String,
+    email: String,
+    password: String
+})
+module.exports = mongoose.model('Users', UserSchema);
